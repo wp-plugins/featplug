9e1810d3118357a2b46671e6695e037caeb00e86
raistlinthewiz <raistlinthewiz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-09-16 10:32:39 +0000

    * releasing 0.62 (fixed 0.61 release)
    git-svn-id: https://plugins.svn.wordpress.org/featplug/tags/0.62@65136 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ featplug-wp.php
@@ -0,0 +1,43 @@
+<?php  
+
+/*
+
+	Plugin Name: Featplug
+	Plugin URI: http://featplug.huseyinuslu.net
+	Description: Featplug is wordpress plugin that can mine your Wordpress site and generate 'featured' section using the found items.
+	Version: 0.62
+	Author: Huseyin Uslu
+	Author URI: http://www.huseyinuslu.net
+
+*/
+
+/*  
+	FeatPlug - http://featplug.huseyinuslu.net
+
+	Featplug is free for non-commercial use. For commercial usage and redistribution with commercial packages
+	check the plugins webpage http://featplug.huseyinuslu.net
+
+	Huseyin Uslu < shalafirasitlin@gmail.com | http://www.huseyinuslu.net >
+*/
+
+include_once 'engine.php'; 
+include_once 'layer/wordpress/featplug-wordpress.php';
+include_once 'layer/wordpress/featplug-widget.php'; 
+
+
+/* administration */ 
+add_action('admin_menu','featplug_wp_admin_menu');  
+
+/* widget */ 
+add_action('plugins_loaded', 'featplug_widget_init');  
+
+function featplug_wp_admin_menu() 
+{ 	
+	if (function_exists('add_options_page')) 
+	{ 			
+		add_options_page('FeatPlug','FeatPlug','manage_options','featplug/layer/wordpress/featplug-wp-options.php'); 			 		
+	} 
+}  
+
+
+?>
---
+++ templates/imagemenu/footer.php
@@ -0,0 +1,23 @@
+<?php
+
+/*  
+	FeatPlug - http://featplug.huseyinuslu.net
+
+	Featplug is free for non-commercial use. For commercial usage and redistribution with commercial packages
+	check the plugins webpage http://featplug.huseyinuslu.net
+
+	Huseyin Uslu < shalafirasitlin@gmail.com | http://www.huseyinuslu.net >
+*/
+
+?>
+
+</ul>
+</div>
+
+<script type="text/javascript">
+	
+window.addEvent('domready', function(){
+var myMenu = new ImageMenu($$('#imageMenu a'),{openWidth:310, border:2, onOpen:function(e,i){window.location=e;}});
+});
+</script>
+
