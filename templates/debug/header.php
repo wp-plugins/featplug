e1d6f127a744fe68ae66fb2800e1441efe533d6a
raistlinthewiz <raistlinthewiz@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-10-15 08:37:23 +0000

    * releasing minor 0.63; - fixed @getimagesize timeout problem with hosts not responding which could lead to waiting a host upto 60secs etc.. - smoothgallery ie7 bugfix: http://smoothgallery.jondesign.net/forums/viewtopic.php?id=1599 - added image cache guess working - smootgallery info zone height css fix
    git-svn-id: https://plugins.svn.wordpress.org/featplug/tags/0.63@68993 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ templates/debug/header.php
@@ -0,0 +1,24 @@
+<?php
+
+/*  
+	FeatPlug - http://featplug.huseyinuslu.net
+
+	Featplug is free for non-commercial use. For commercial usage and redistribution with commercial packages
+	check the plugins webpage http://featplug.huseyinuslu.net
+
+	Huseyin Uslu < shalafirasitlin@gmail.com | http://www.huseyinuslu.net >
+*/
+
+?>
+
+<h1>featplug debug template:</h1><br>
+
+<?php echo "Template Path: $this->template_path"; ?>
+
+<table border="1">
+<tr>
+<td>Label</td>
+<td>Content</td>
+<td>Image</td>
+<td>Thumbnail</td>
+</tr>
---
+++ templates/imagemenu/loop.php
@@ -0,0 +1,14 @@
+<?php
+
+/*  
+	FeatPlug - http://featplug.huseyinuslu.net
+
+	Featplug is free for non-commercial use. For commercial usage and redistribution with commercial packages
+	check the plugins webpage http://featplug.huseyinuslu.net
+
+	Huseyin Uslu < shalafirasitlin@gmail.com | http://www.huseyinuslu.net >
+*/
+
+?>
+
+<li><a href="<?php echo $item->link;?>" style="background: url(<?php echo $item->image;?>) repeat scroll 0%;" alt="<?php echo $item->label;?>" title="<?php echo $item->label;?>"><?php echo $item->label;?></a></li>
